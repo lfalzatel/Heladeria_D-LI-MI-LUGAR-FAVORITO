c7a99b1d70bd8f6a247f9b286d9179b5bdd53592
lfalzatel <lfalzatel@gmail.com>
2026-04-25 18:58:33 -0500

    Fix: habilitar instalación PWA agregando start_url y scope

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.aegmgc0d4e4"
+    "revision": "0.7ilem2s079"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
