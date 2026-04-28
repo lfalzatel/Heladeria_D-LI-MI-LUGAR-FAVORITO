0c66b7ff9e7f0ceea1548d0dc47ce2fc959fc759
lfalzatel <lfalzatel@gmail.com>
2026-04-28 14:45:02 -0500

    fix infinite loop

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.cbusgvesno4"
+    "revision": "0.54jbp8r2pe8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { registerSW } from 'virtual:pwa-register';
-
-// Register service worker for PWA
-registerSW({ immediate: true });
 
 // Global capture for PWA install prompt
 (window as any).deferredPrompt = null;
