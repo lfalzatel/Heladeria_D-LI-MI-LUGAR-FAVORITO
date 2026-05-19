7895df765765330ee5bdbf420d2624e5f3845917
lfalzatel <lfalzatel@gmail.com>
2026-05-18 19:41:22 -0500

    Fix: separar Cono y Vaso en productos independientes en el JSON

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -33,7 +33,7 @@ export default function MainLayout() {
           />
         )}
 
-        <div className="flex-1 flex flex-col min-w-0 overflow-x-hidden">
+        <div className="flex-1 flex flex-col min-w-0 overflow-x-hidden pb-24 lg:pb-0">
           <Outlet />
         </div>
 
